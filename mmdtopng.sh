d7d8d7ddca64e5cf2afa963bd3f3b50999b8f873
Iosif Takakura <takakura.yusuke@gmail.com>
2024-08-30 17:43:43 +0900

    補正

--- mmdtopng.sh
+++ mmdtopng.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
-
-while [ -n "$1" ]
-do
-    fullname=$1
-    extension=${fullname##*.}
-    filename=${fullname%.*}
-    if [ extension == "mermaid" ] ; then
-        mmdc -i ${filename}".mermaid" -o ${filename}".png" -t neutral -b transparent
-    fi
-    shift
+[ "x$1" = "x" ] && exit 1
+DIR=$1
+for file in `\find $DIR -name '*.mermaid'`; do
+    filename=${file%.*}
+    mmdc -i ${filename}".mermaid" -o ${filename}".png" -t neutral -b transparent
 done
--- svgtopng.sh
+++ svgtopng.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
-
-while [ -n "$1" ]
-do
-    fullname=$1
-    extension=${fullname##*.}
-    filename=${fullname%.*}
-    if [ extension == "svg"  ] ; then
-        convert -density 1200 ${filename}".svg" ${filename}".png"
-    fi
-    shift
+[ "x$1" = "x" ] && exit 1
+DIR=$1
+for file in `\find $DIR -name '*.svg'`; do
+    filename=${file%.*}
+    convert -density 1200 ${filename}".svg" ${filename}".png"
 done
