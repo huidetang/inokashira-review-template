7c36f5cbd37d680fc0d3f4af626795339401c73b
Iosif Takakura <takakura.yusuke@gmail.com>
2024-08-30 18:11:58 +0900

    fix.

--- mmdtopng.sh
+++ mmdtopng.sh
@@ -3,5 +3,5 @@
 DIR=$1
 for file in `\find $DIR -name '*.mermaid'`; do
     filename=${file%.*}
-    mmdc -i ${filename}".mermaid" -o ${filename}".png" -t neutral -b transparent
+    mmdc -i ${filename}".mermaid" -o ${filename}".png" -t neutral -b transparent --no-sandbox
 done
