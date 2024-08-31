4519af1cde2af6159db23f15b72c94af991ed0da
Iosif Takakura <takakura.yusuke@gmail.com>
2024-09-01 08:34:59 +0900

    update.

--- mmdtopng.sh
+++ mmdtopng.sh
@@ -3,5 +3,5 @@
 DIR=$1
 for file in `\find $DIR -name '*.mermaid'`; do
     filename=${file%.*}
-    mmdc --puppeteerConfigFile puppeteerConfig.json -i ${filename}".mermaid" -o ${filename}".png" -t neutral -b transparent
+    pnpm run mmdc --puppeteerConfigFile puppeteerConfig.json -i ${filename}".mermaid" -o ${filename}".png" -t neutral -b transparent
 done
